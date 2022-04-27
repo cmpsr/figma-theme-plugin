ee8a7372f73da4c12e10e50ce9ed6e8735bf7415
Pau Teruel Soldevila <93.pau.teruel@gmail.com>
2022-04-27 17:38:27 +0200

    feat: add basic notifications when theme has been extracted

--- src/code.ts
+++ src/code.ts
@@ -5,38 +5,45 @@ import { Spacings } from './spacings';
 import { Texts } from './texts';
 import { TextsPairings } from './textsPairings';
 
-// Extract colors
-const colors = new Colors().get();
+try {
+  // Extract colors
+  const colors = new Colors().get();
 
-// Extract spacings
-const spacings = new Spacings().get();
+  // Extract spacings
+  const spacings = new Spacings().get();
 
-// Extract radiius
-const radius = new Radius().get();
+  // Extract radiius
+  const radius = new Radius().get();
 
-// Extract elevations
-const elevations = new Elevations().get();
+  // Extract elevations
+  const elevations = new Elevations().get();
 
-// Extract texts
-const texts = new Texts().get();
+  // Extract texts
+  const texts = new Texts().get();
 
-// Extract textsPairings
-const textsPairings = new TextsPairings().get();
+  // Extract textsPairings
+  const textsPairings = new TextsPairings().get();
 
-console.log({
-  colors,
-  space: spacings,
-  radii: radius,
-  shadows: elevations,
-  textStyles: texts,
-  components: {
-    TextPairing: textsPairings,
-  },
-});
+  console.log({
+    colors,
+    space: spacings,
+    radii: radius,
+    shadows: elevations,
+    textStyles: texts,
+    components: {
+      TextPairing: textsPairings,
+    },
+  });
+
+  figma.notify('Theme has been extracted successfully ✅', { timeout: 3000 });
+} catch (error) {
+  figma.notify('Something went wrong', { timeout: 3000, error: true });
+} finally {
+  figma.closePlugin();
+}
+
 // figma.showUI(__html__, { visible: false });
 // figma.ui.postMessage({
 //   action: 'download',
 //   payload: { colors },
 // });
-
-// figma.closePlugin();

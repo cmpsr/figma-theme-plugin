91bb80eaf8854516352a17ed757bc9545bef00b6
Pau Teruel <93.pau.teruel@gmail.com>
2024-02-06 13:43:12 +0100

    feat: handle responsive text variants in textpairings and postprocess them to simplify the output

--- src/plugin/textsPairings.ts
+++ src/plugin/textsPairings.ts
@@ -8,58 +8,74 @@ export class TextsPairings extends Page {
   }
 
   public get() {
+    const themePrefixRegex = new RegExp(
+      `${THEME_PREFIXES.TEXTS_PAIRINGS.DESKTOP}|${THEME_PREFIXES.TEXTS_PAIRINGS.MOBILE}|${THEME_PREFIXES.TEXTS_PAIRINGS.DEFAULT}`
+    );
+    const textsPrefixRegex = new RegExp(
+      `${THEME_PREFIXES.TEXTS.DESKTOP}|${THEME_PREFIXES.TEXTS.MOBILE}|${THEME_PREFIXES.TEXTS.DEFAULT}`
+    );
+
     this.traversePage((node: FrameNode) => {
       if (this.nodeStartsWithPrefix(node.name, THEME_PREFIXES.TEXTS_PAIRINGS.DEFAULT)) {
-        const textPairingNodeNameNormalized = normalizeTextSuffixToken(node.name);
-        const textPairingToken = textPairingNodeNameNormalized.replace(
-          new RegExp(
-            `${THEME_PREFIXES.TEXTS_PAIRINGS.DESKTOP}|${THEME_PREFIXES.TEXTS_PAIRINGS.MOBILE}|${THEME_PREFIXES.TEXTS_PAIRINGS.DEFAULT}`
-          ),
-          ''
-        );
-        const labelTextNode = (node.children[0] as InstanceNode).children[0] as TextNode;
-        const labelTextNodeNameNormalized = normalizeTextSuffixToken(labelTextNode.name);
-
-        const labelNodeToken = labelTextNodeNameNormalized.replace(
-          new RegExp(`${THEME_PREFIXES.TEXTS.DESKTOP}|${THEME_PREFIXES.TEXTS.MOBILE}|${THEME_PREFIXES.TEXTS.DEFAULT}`),
-          ''
-        );
-        const subLabelTextNode = (node.children[1] as InstanceNode).children[0] as TextNode;
-        const subLabelTextNodeNameNormalized = normalizeTextSuffixToken(subLabelTextNode.name);
-
-        const subLabelNodeToken = subLabelTextNodeNameNormalized.replace(
-          new RegExp(`${THEME_PREFIXES.TEXTS.DESKTOP}|${THEME_PREFIXES.TEXTS.MOBILE}|${THEME_PREFIXES.TEXTS.DEFAULT}`),
-          ''
-        );
-        const isDesktopText = node.name.startsWith(THEME_PREFIXES.TEXTS_PAIRINGS.DESKTOP);
-        const isMobileText = node.name.startsWith(THEME_PREFIXES.TEXTS_PAIRINGS.MOBILE);
+        const textPairingToken = normalizeTextSuffixToken(node.name).replace(themePrefixRegex, '');
 
         if (!this.data[textPairingToken]) {
           this.data[textPairingToken] = {
-            label: { variant: labelNodeToken },
-            subLabel: { variant: subLabelNodeToken },
+            label: { variant: {} },
+            subLabel: { variant: {} },
             columnGap: {},
           };
         }
 
+        // Determine the responsive size suffix based on the node name.
+        const sizeSuffix = node.name.startsWith(THEME_PREFIXES.TEXTS_PAIRINGS.DESKTOP) ? 'md' : 'base';
         const itemSpacing = convertPxToRem(node.itemSpacing);
 
-        if (isDesktopText) {
-          this.data[textPairingToken].columnGap.md = itemSpacing;
-        } else if (isMobileText) {
-          this.data[textPairingToken].columnGap.base = itemSpacing;
-        } else {
-          this.data[textPairingToken] = {
-            ...this.data[textPairingToken],
-            columnGap: {
-              base: itemSpacing,
-              md: itemSpacing,
-            },
-          };
-        }
+        ['label', 'subLabel'].forEach((type, index) => {
+          const textNode = (node.children[index] as InstanceNode).children[0] as TextNode;
+          const textToken = normalizeTextSuffixToken(textNode.name).replace(textsPrefixRegex, '');
+
+          this.data[textPairingToken][type].variant[sizeSuffix] = textToken;
+        });
+
+        this.data[textPairingToken].columnGap[sizeSuffix] = itemSpacing;
       }
     });
 
+    this.simplifyDataStructure();
+
     return { parts: ['label', 'subLabel', 'container'], variants: this.data };
+  }
+
+  // Simplifies the data structure by reducing variants to single values when identical across breakpoints or when only one breakpoint value is present.
+  private simplifyDataStructure() {
+    Object.keys(this.data).forEach((token) => {
+      Object.keys(this.data[token]).forEach((property) => {
+        const item = this.data[token][property];
+        let simplifiedValue: string | undefined;
+
+        // Determine if the current item is a variant or directly a gap definition.
+        if (item.variant) {
+          simplifiedValue = this.getSimplifiedValue(item.variant.base, item.variant.md);
+        } else {
+          simplifiedValue = this.getSimplifiedValue(item.base, item.md);
+        }
+
+        if (simplifiedValue !== undefined) {
+          this.data[token][property] = simplifiedValue;
+        }
+      });
+    });
+  }
+
+  private getSimplifiedValue(base: string | undefined, md: string | undefined): string | undefined {
+    // Return the value directly if both are the same or if only one is provided.
+    if (base === md || !md) {
+      return base;
+    } else if (!base) {
+      return md;
+    }
+    // Return undefined if no simplification is possible (both values differ).
+    return undefined;
   }
 }
